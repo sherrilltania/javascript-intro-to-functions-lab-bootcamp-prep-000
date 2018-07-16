22bad466898660adc2e06b5e9d84d2ff5bad3ad0
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-16 12:17:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-function logShout(string){
+function toShout(string){
   console.log(string.toUpperCase())
-}
-function logWhisper(string){
-  console.log(string.toLowerCase())
-}
-function sayHiToGrandma(string){
-  if (string=string.toLowerCase())
-  return `I can\'t hear you!`
-}
-function sayHiToGrandma(string){
-  if(string=string.toUpperCase())
-  return `YES INDEED!`
 }
