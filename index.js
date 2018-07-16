c7c08d9e524e52a8a273220c0c216484b1f05cb8
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-16 12:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string=string.toLowerCase())
-  return `I can\'t hear you!`
+  console.log("I can\'t hear you!")
 } else if(string=string.toUpperCase()){
-  return `YES INDEED!`
+  console.log("YES INDEED!")
 } else if(string="I love you, Grandma."){
-  return `I love you, too.`
+  console.log("I love you, too.")
 }
